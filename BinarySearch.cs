6f2774eeb506639b8f15c069fef952f50b985cf9
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-06-01 19:22:13 +0530

    [Biplab] Add: Binary tree

---
+++ BinarySearch.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Text;
+//using System.Linq;
+//using System.Threading.Tasks;
+namespace BinarySearchTree
+{
+    internal class BinarySearch<K> where K : IComparable
+    { 
+        public K NodeData;
+        public BinarySearch<K> Left;
+        public BinarySearch<K> Right;
+        public BinarySearch(K Nodedata)
+        {
+            this.NodeData = Nodedata;
+            this.Left = null;
+            this.Right = null;
+        }
+        public void AddNode(K item)
+        {
+            K CurrentNode = NodeData;   //56
+            if (item.CompareTo(CurrentNode) < 0)
+            {
+                if (this.Left == null)
+                {
+                    Left = new BinarySearch<K>(item);
+                }
+                else
+                {
+                    Left.AddNode(item);
+                }
+
+            }
+            else
+            {
+                if (this.Right == null)
+                {
+                    Right = new BinarySearch<K>(item);
+                }
+                else
+                {
+                    Right.AddNode(item);
+                }
+
+            }
+        }
+        public void Display()
+        {
+            if (this.Left != null)
+            {
+                Left.Display();
+            }
+            Console.Write("  ");
+            Console.Write(this.NodeData);
+            Console.Write("  ");
+            if (this.Right != null)
+            {
+                Right.Display();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Binary Search Tree");
+            BinarySearch<int> binarySearch = new BinarySearch<int>(56);
+            binarySearch.AddNode(30);
+            binarySearch.AddNode(70);
+            binarySearch.Display();
+
+        }
+    }
+    
+}
