61c8482c1508bd880a84ac7bd546dc8b519142c1
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-06-01 19:59:17 +0530

    [Biplab] Add: Binary tree size

--- BinarySearch.cs
+++ BinarySearch.cs
@@ -5,58 +5,65 @@ using System.Collections.Generic;
 //using System.Threading.Tasks;
 namespace BinarySearchTree
 {
-    internal class BinarySearch<K> where K : IComparable
-    { 
-        public K NodeData;
-        public BinarySearch<K> Left;
-        public BinarySearch<K> Right;
-        public BinarySearch(K Nodedata)
+
+        public class BinarySearch<K> where K : IComparable
         {
-            this.NodeData = Nodedata;
-            this.Left = null;
-            this.Right = null;
-        }
-        public void AddNode(K item)
-        {
-            K CurrentNode = NodeData;   //56
-            if (item.CompareTo(CurrentNode) < 0)
+            public K NodeData;
+            public BinarySearch<K> Left;
+            public BinarySearch<K> Right;
+            public BinarySearch(K Nodedata)
             {
-                if (this.Left == null)
+                this.NodeData = Nodedata;
+                this.Left = null;
+                this.Right = null;
+            }
+            int LeftCount = 0;
+            int RightCount = 0;
+            public void AddNode(K item)
+            {
+                K CurrentNode = NodeData;   //56
+                if (item.CompareTo(CurrentNode) < 0)
                 {
-                    Left = new BinarySearch<K>(item);
+                    if (this.Left == null)
+                    {
+                        Left = new BinarySearch<K>(item);
+                    }
+                    else
+                    {
+                        Left.AddNode(item);
+                    }
+                    LeftCount++;
                 }
                 else
                 {
-                    Left.AddNode(item);
+                    if (this.Right == null)
+                    {
+                        Right = new BinarySearch<K>(item);
+                    }
+                    else
+                    {
+                        Right.AddNode(item);
+                    }
+                    RightCount++;
                 }
-
             }
-            else
+            public void Getsize()
             {
-                if (this.Right == null)
+                Console.WriteLine("Size of the Binary Tree :" + (LeftCount + RightCount + 1));
+            }
+            public void Display()
+            {
+                if (this.Left != null)
                 {
-                    Right = new BinarySearch<K>(item);
+                    Left.Display();
                 }
-                else
+                Console.Write("  ");
+                Console.Write(this.NodeData);
+                Console.Write("  ");
+                if (this.Right != null)
                 {
-                    Right.AddNode(item);
+                    Right.Display();
                 }
-
-            }
-        }
-        public void Display()
-        {
-            if (this.Left != null)
-            {
-                Left.Display();
-            }
-            Console.Write("  ");
-            Console.Write(this.NodeData);
-            Console.Write("  ");
-            if (this.Right != null)
-            {
-                Right.Display();
             }
         }
     }
-}
--- Program.cs
+++ Program.cs
@@ -6,13 +6,23 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             Console.WriteLine("Welcome to Binary Search Tree");
-            BinarySearch<int> binarySearch = new BinarySearch<int>(56);
-            binarySearch.AddNode(30);
-            binarySearch.AddNode(70);
-            binarySearch.Display();
-
+            BinarySearch<int> binarySearchTree = new BinarySearch<int>(56);
+            binarySearchTree.AddNode(30);
+            binarySearchTree.AddNode(70);
+            binarySearchTree.AddNode(22);
+            binarySearchTree.AddNode(40);
+            binarySearchTree.AddNode(60);
+            binarySearchTree.AddNode(95);
+            binarySearchTree.AddNode(11);
+            binarySearchTree.AddNode(65);
+            binarySearchTree.AddNode(3);
+            binarySearchTree.AddNode(16);
+            binarySearchTree.AddNode(67);
+            binarySearchTree.AddNode(63);
+            binarySearchTree.Display();
+            Console.WriteLine("\n");
+            binarySearchTree.Getsize();
         }
     }
     
